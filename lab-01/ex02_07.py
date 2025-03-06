55c731a2ce7eebcecaea9e78d2058a18e244ec02
thnkthuhigh <a01204496068@gmail.com>
2025-03-06 15:21:32 +0700

    done

--- lab-01/ex02_07.py
+++ lab-01/ex02_07.py
@@ -1,1 +1,10 @@
-print("Nhap cac dong va ban (Nhap 'done' de ket thuc):")
+print("nhap cac dong van ban(nhap'done' de ket thuc):")
+lines=[]
+while True:
+    line=input()
+    if line.lower()=='done':
+        break
+    lines.append(line)
+print("\nCac dong da nhap sau khi chuyen thanh chu in hoa:")
+for line in lines:
+    print(line.upper())
