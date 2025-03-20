fff5c9fda2b7c7730dfbbc1481af85608cf99319
thnkthuhigh <132247739+thnkthuhigh@users.noreply.github.com>
2025-03-20 15:29:38 +0700

    Delete lab-01 directory

--- lab-01/ex02/ex02_02.py
+++
@@ -1,2 +0,0 @@
-
-print("Dien tich hinh tron la: ", 3.14 *(float(input("Nhap ban kinh hinh tron: ")) ** 2))
--- lab-01/ex02/ex02_03.py
+++
@@ -1,5 +0,0 @@
-so = int(input("Nhap 1 so nguyen: "));
-if so % 2 == 0:
-    print(so, "la so chan.")
-else:
-    print(so, "khong phai la so chan.")
